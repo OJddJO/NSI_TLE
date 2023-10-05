da593d316f9ada680aafddff9a1fb8288cd8b0f2
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-10-05 19:40:56 +0200

    a

---
+++ piles.py
@@ -0,0 +1,93 @@
+class Pile:
+    """Pile represents a pile of n object, where n is the size of the pile.
+
+    Elements are added to the top of the pile and removed from the top of the pile.
+    The pile is empty when the top is 0.
+
+    An empty slot of a pile is represented by None.
+
+    >>> myPile = Pile(5)
+    >>> print(myPile)
+    [None, None, None, None, None]
+    
+    >>> myPile.add(1)
+    1
+    >>> print(myPile)
+    [1, None, None, None, None]"""
+    def __init__(self, size: int) -> None:
+        self._size = size
+        self._pile = [None] * size
+        self._top = 0
+
+    def emptyPile(self) -> bool:
+        """Returns True if the pile is empty."""
+        return self._top == 0
+
+    def add(self, element) -> object:
+        """Adds an element to the top of the pile."""
+        if self._top == self._size:
+            raise IndexError("Pile is full.")
+        else:
+            self._pile[self._top] = element
+            self._top += 1
+            return element
+
+    def remove(self) -> object:
+        """Removes the top element from the pile."""
+        if self.emptyPile():
+            raise IndexError("Pile is empty.")
+        else:
+            self._top -= 1
+            self._pile[self._top] = None
+            return self._pile[self._top]
+
+    def __str__(self) -> str:
+        """Returns a string representation of the pile."""
+        return str(self._pile)
+
+    def __len__(self) -> int:
+        """Returns the size of the pile."""
+        return self._size
+
+    def __iter__(self) -> object:
+        """Returns an iterator for the pile."""
+        return _PileIterator(self._pile, self._top)
+
+
+class _PileIterator:
+    """An iterator for the pile."""
+    def __init__(self, pile, top):
+        self._pile = pile
+        self._top = top
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < self._top:
+            element = self._pile[self._index]
+            self._index += 1
+            return element
+        else:
+            raise StopIteration
+
+
+if __name__ == '__main__':
+    pile = Pile(5)
+    print(pile.emptyPile())
+    pile.add(1)
+    pile.add(2)
+    pile.add(3)
+    print(pile)
+    print(pile.remove())
+    print(pile)
+    print(pile.emptyPile())
+    print(len(pile))
+    for element in pile:
+        print(element)
+    print(pile)
+    pile.add(4)
+    pile.add(5)
+    print(pile)
+    print(pile.remove())
---
+++ queues.py
@@ -0,0 +1,109 @@
+class Queue:
+    """Queue represent a queue data structure of n element, where n is the size of the queue.
+    
+    Elements are added to the rear of the queue and removed from the front of the queue.
+    The queue is empty when the front and rear are the same.
+    
+    >>> myQueue = Queue(5)
+    >>> print(myQueue)
+    [None, None, None, None, None]
+    
+    >>> myQueue.add(1)
+    1
+    >>> print(myQueue)
+    [1, None, None, None, None]
+    
+    >>> myQueue.add(2)
+    2
+    
+    >>> myQueue.remove()
+    1
+    >>> print(myQueue)
+    [1, 2, None, None, None] # Note that the first element is still 1
+    >>> print(myQueue.getQueue())
+    [2]"""
+    def __init__(self, size: int):
+        self._size = size
+        self._queue = [None] * size
+        self._start = 0
+        self._end = 0
+    
+    def emptyQueue(self) -> bool:
+        """Returns True if the queue is empty."""
+        return self._start == self._end
+    
+    def fullQueue(self) -> bool:
+        """Returns True if the queue is full."""
+        return self._start == self._size
+    
+    def add(self, element) -> object:
+        """Adds an element to the rear of the queue."""
+        if self.fullQueue():
+            raise IndexError("Queue is full.")
+        else:
+            self._queue[self._end] = element
+            self._end += 1
+            return element
+
+    def remove(self) -> object:
+        """Removes the front element from the queue."""
+        if self.emptyQueue():
+            raise IndexError("Queue is empty.")
+        else:
+            element = self._queue[self._start]
+            self._start += 1
+            return element
+        
+    def getQueue(self) -> list:
+        """Returns the queue."""
+        return self._queue[self._start:self._end]
+        
+    def __str__(self) -> str:
+        """Returns a string representation of the queue."""
+        return str(self._queue)
+    
+    def __len__(self) -> int:
+        """Returns the size of the queue."""
+        return self._size
+    
+    def __iter__(self) -> object:
+        """Returns an iterator for the queue."""
+        return _QueueIterator(self._queue, self._start, self._end)
+
+
+class _QueueIterator:
+    """An iterator for the queue."""
+    def __init__(self, queue, start, end):
+        self._start = start
+        self._end = end
+        self._queue = queue[self._start:self._end]
+        self._index = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self._index < self._end - self._start:
+            element = self._queue[self._index]
+            self._index += 1
+            return element
+        else:
+            raise StopIteration
+
+
+if __name__ == '__main__':
+    queue = Queue(5)
+    print("Queue is empty:", queue.emptyQueue())
+    print("Queue is full:", queue.fullQueue())
+    print("Adding 1 to queue:", queue.add(1))
+    print("Adding 2 to queue:", queue.add(2))
+    print("Adding 3 to queue:", queue.add(3))
+    print("Adding 4 to queue:", queue.add(4))
+    print("Queue is empty:", queue.emptyQueue())
+    print("Queue is full:", queue.fullQueue())
+    print("Removing from queue:", queue.remove())
+    print(queue)
+    print("Print all element in queue")
+    print(queue.getQueue())
+    for i in queue:
+        print(i)
